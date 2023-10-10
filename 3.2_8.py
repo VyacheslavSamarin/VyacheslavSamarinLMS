abd12123b9e89df61e9bb1ea310e83a838abf51b
VyacheslavSamarin <slava.samarin.95@mail.ru>
2023-10-10 22:56:49 +0300

    hi

--- 3.2_8.py
+++ 3.2_8.py
@@ -2,8 +2,9 @@ a = int(input())
 s = {}
 for _ in range(a):
     x = input().split()
-    if x[1] not in s:
-        s[x[1]] = []
-    s[x[1]].append(x[0])
+    for i in range(1, len(x)):
+        if x[i] not in s:
+            s[x[i]] = []
+        s[x[i]].append(x[0])
 b = input()
 print(*sorted(s[b]), sep='\n') if b in s else print('Таких нет')
