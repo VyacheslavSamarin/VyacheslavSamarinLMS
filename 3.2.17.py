744752f4405864557fdc798c5a0494b92942772b
VyacheslavSamarin <slava.samarin.95@mail.ru>
2023-10-11 21:16:51 +0300

    LMS

--- 3.2.17.py
+++ 3.2.17.py
@@ -1,9 +1,16 @@
 a, z = int(input()), {}
 for _ in range(a):
-    x, y = input(), input()
-    h = str(len(x)) + ' ' + str(len(y))
+    x = input().split()
+    y = x[1]
+    x = x[0]
+    h, f = x[:-1] + ' ' + y[:-1], [x, y] 
     if h not in z:
         z[h] = []
-    b = [x, y]
-    z[h].append(b)
-print(z)
+    z[h].append(f)
+m = -1
+for x in z.values():
+    m = max(m, len(x))
+print(m)
+
+
+    
